6395750e64565378732842285db52ebcfe214aa0
JP Hastings-Spital <jphastings@gmail.com>
2018-12-30 14:25:40 +0000

    Try stepper

--- periph.go
+++ periph.go
@@ -15,12 +15,29 @@ func main() {
 		log.Fatal(err)
 	}
 
-	t := time.NewTicker(500 * time.Millisecond)
-	for l := gpio.Low; ; l = !l {
-		// Lookup a pin by its location on the board:
-		if err := rpi.P1_33.Out(l); err != nil {
+	motorNotSleep := rpi.P1_27
+	pinDirC := rpi.P1_7
+	pinStepC := rpi.P1_11
+
+	if err := motorNotSleep.Out(gpio.High); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := pinDirC.Out(gpio.Low); err != nil {
+		log.Fatal(err)
+	}
+
+	t := time.NewTicker(time.Second)
+	for {
+		if err := pinStepC.Out(gpio.Low); err != nil {
 			log.Fatal(err)
 		}
 		<-t.C
+
+		if err := pinStepC.Out(gpio.High); err != nil {
+			log.Fatal(err)
+		}
+
+		<-time.NewTimer(200 * time.Millisecond).C
 	}
 }
