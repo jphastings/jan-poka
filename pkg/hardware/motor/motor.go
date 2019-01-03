6555e93f57d79f881c177f1c4fc666c6c7c0429f
JP Hastings-Spital <jphastings@gmail.com>
2019-01-03 00:14:09 +0000

    Fix over-eager gitignore

---
+++ pkg/hardware/motor/motor.go
@@ -0,0 +1,46 @@
+package motor
+
+import (
+	"fmt"
+	. "github.com/jphastings/corviator/pkg/math"
+	"periph.io/x/periph/conn/gpio"
+	"strings"
+)
+
+type Motor struct {
+	StepChannel chan bool
+	Angle       Degrees
+
+	directionPin gpio.PinIO
+	stepPin      gpio.PinIO
+}
+
+func New(angle Degrees, directionPin, stepPin gpio.PinIO) *Motor {
+	m := &Motor{
+		StepChannel: make(chan bool),
+		Angle:       angle,
+
+		directionPin: directionPin,
+		stepPin:      stepPin,
+	}
+
+	i := int(angle / 120)
+	go m.Move(string(rune(i + 65)))
+
+	return m
+}
+
+func (m *Motor) Move(name string) {
+	for {
+		select {
+		case isForward := <-m.StepChannel:
+			if isForward {
+				// directionPin = gpio.High
+				fmt.Printf(name)
+			} else {
+				// directionPin = gpio.Low
+				fmt.Printf(strings.ToLower(name))
+			}
+		}
+	}
+}
