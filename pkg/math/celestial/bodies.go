30eda4ecb4142ce8a056b845ee525b7821c83b51
JP Hastings-Spital <jphastings@gmail.com>
2019-01-05 01:49:30 +0000

    C wrapper library for Celestial positions
    Uses libnova for calculating rough positions for the major celestial bodies.

---
+++ pkg/math/celestial/bodies.go
@@ -0,0 +1,16 @@
+package celestial
+
+type Body string
+
+const (
+	Moon    Body = "moon"
+	Sun     Body = "sun"
+	Mercury Body = "mercury"
+	Venus   Body = "venus"
+	Mars    Body = "mars"
+	Jupiter Body = "jupiter"
+	Saturn  Body = "saturn"
+	Neptune Body = "neptune"
+	Uranus  Body = "uranus"
+	Pluto   Body = "pluto"
+)
